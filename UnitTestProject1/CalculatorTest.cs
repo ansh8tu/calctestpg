509fbce7f3dabbe4a9b173c43b84e320b7ebc223
itayhau <itayhau@gmail.com>
2021-02-12 18:03:22 +0200

    Update CalculatorTest.cs

--- UnitTestProject1/CalculatorTest.cs
+++ UnitTestProject1/CalculatorTest.cs
@@ -8,46 +8,6 @@ namespace UnitTestProject1
     [TestClass]
     public class CalculatorTest
     {
-        [AssemblyInitialize]
-        public static void TestDbConnection(TestContext tx)
-        {
-            try
-            {
-                using (var my_conn = new NpgsqlConnection("Host = localhost; Username = postgres; Password = admin; Database = postgres"))
-                {
-                    my_conn.Open();
-                }
-            }
-            catch (Exception ex)
-            {
-                // write error to log file
-                throw new ApplicationException("No db connection valid....");
-            }
-        }
-
-        [TestMethod]
-        public void Foo()
-        {
-            using (var conn = new NpgsqlConnection("Host = localhost; Username = postgres; Password = admin; Database = postgres"))
-            {
-                conn.Open();
-                string query = "SELECT * FROM movies";
-
-                NpgsqlCommand command = new NpgsqlCommand(query, conn);
-                command.CommandType = System.Data.CommandType.Text; // this is default
-
-                var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    long id = (long)reader["id"];
-                    string title = (string)reader["title"];
-                    // ....
-                    Console.WriteLine($"{id} {title}");
-                }
-            }
-
-        }
-
         [TestMethod]
         public void Calculator_Add_Sum_Regular_Numbers()
         {
